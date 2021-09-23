1ec5ba4191ed6993e767c7b3f0530e925facc604
ameyajangam22 <ameya.g.jangam@gmail.com>
2021-09-23 11:16:16 +0530

    Changed logo icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import { ReactComponent as MemeifyLogoLight } from './icons/logo-light.svg';
 import { ReactComponent as MemeifyLogoDark } from './icons/logo-dark.svg';
-import { ChakraProvider, Box, VStack, Grid, theme } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  Box,
+  VStack,
+  Grid,
+  Container,
+  theme,
+} from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
@@ -9,19 +16,21 @@ function App() {
   const [colourTheme, setColourTheme] = useState('');
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher
-            justifySelf="flex-end"
-            giveTheme={val => setColourTheme(val)}
-          />
-          <VStack spacing={8}>
-            {colourTheme == 'light' ? (
+      <Box fontSize="xl">
+        <Grid templateColumns="repeat(2, 1fr)" p={3}>
+          <Box h="10" maxW="xs">
+            {colourTheme === 'light' ? (
               <MemeifyLogoLight />
             ) : (
               <MemeifyLogoDark />
             )}
-          </VStack>
+          </Box>
+          <Box w="100%" h="10">
+            <ColorModeSwitcher
+              justifySelf="flex-end"
+              giveTheme={val => setColourTheme(val)}
+            />
+          </Box>
         </Grid>
       </Box>
     </ChakraProvider>
